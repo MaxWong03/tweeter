93533f0c56d75c4d308db6b6e3fc9a11e2349216
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-10 20:21:13 +0000

    added and updated word counters

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.new-tweet textarea').on('input', function(event) {
+    const {textLength} = $(this).context;
+    const count = $(this).parent().children('.counter');
+    count.text(140 - textLength);
+    140 - textLength < 0 ? count.css('color', 'red') : count.css('color', '');
+  });
+  
+});
