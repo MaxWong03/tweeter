e0f2a64728901bc5b026c9664823721f57a24004
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-11 03:48:39 +0000

    style tweet container, used ajax on client js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,68 +5,73 @@
  */
 
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
-const renderTweets = (tweets) => {
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('#tweet-container').append($tweet);
-  }
-};
-
-
-const createTweetElement = (tweetObj) => {
-  const {name, avatars, handle} = tweetObj.user;
-  const {text} = tweetObj.content;
-  const timeStamp = tweetObj.created_at;
-  return `<article class="tweet">
-  <header>
-    <img class="avatar light" src=${avatars}>
-    <div class="userInfo">
-      <span class="light">${name}</span>
-      <small class="user-handle hide">${handle}</small>
-    </div>
-  </header>
-  <div class="tweet-text">
-    <p class="light">${text}</p>
-  </div>
-  <footer>
-    <small class=light>${Math.round((Date.now() - new Date(timeStamp)) / (1000 * 60 * 60 * 24))} Days Ago</small>
-    <span class="reaction light">
-      <i class="fas fa-flag"></i>
-      <i class="fas fa-retweet"></i>
-      <i class="fas fa-heart"></i>
-
-    </span>
-  </footer>
-</article>`;
-};
 
 
 $(document).ready(function() {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+  
+  
+  const renderTweets = (tweets) => {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweet-container').append($tweet);
+    }
+  };
+  
+  
+  const createTweetElement = (tweetObj) => {
+    const {name, avatars, handle} = tweetObj.user;
+    const {text} = tweetObj.content;
+    const timeStamp = tweetObj.created_at;
+    return `<article class="tweet">
+    <header>
+      <img class="avatar light" src=${avatars}>
+      <div class="userInfo">
+        <span class="light">${name}</span>
+        <small class="user-handle hide">${handle}</small>
+      </div>
+    </header>
+    <div class="tweet-text">
+      <p class="light">${text}</p>
+    </div>
+    <footer>
+      <small class=light>${Math.round((Date.now() - new Date(timeStamp)) / (1000 * 60 * 60 * 24))} Days Ago</small>
+      <span class="reaction light">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+  
+      </span>
+    </footer>
+  </article>`;
+  };
+
+  const loadTweets = (tweet) => {
+    
+  };
+
   renderTweets(data);
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-  $('.new-tweet textarea').on('keyup', function(event) {
-    const {textLength} = $(this).context;
+$(document).ready(function () {
+  $('.new-tweet textarea').on('keyup', function (event) {
+    const { textLength } = $(this).context;
     const count = $(this).parent().children('.counter');
     count.text(140 - textLength);
     140 - textLength < 0 ? count.css('color', 'red') : count.css('color', '');
   });
-  
+
+  $('form').on('submit', function (event) {
+    event.preventDefault();
+    $.ajax('/tweets', {
+      method: "POST",
+      data: $(this).serialize()
+    }).then(response => console.log('response is', response));
+  });
 });
