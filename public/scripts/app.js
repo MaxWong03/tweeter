6fda44c91538af439db983aab2a3e77e0dea678e
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-11 19:06:20 +0000

    refactor in es6 format

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,44 +7,18 @@
 
 
 
-$(document).ready(function() {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  
-  
+$(document).ready(function () {
+
   const renderTweets = (tweets) => {
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $('#tweet-container').append($tweet);
     }
   };
-  
-  
+
   const createTweetElement = (tweetObj) => {
-    const {name, avatars, handle} = tweetObj.user;
-    const {text} = tweetObj.content;
+    const { name, avatars, handle } = tweetObj.user;
+    const { text } = tweetObj.content;
     const timeStamp = tweetObj.created_at;
     return `<article class="tweet">
     <header>
@@ -69,9 +43,11 @@ $(document).ready(function() {
   </article>`;
   };
 
-  const loadTweets = (tweet) => {
-    
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: "GET" })
+      .then(renderTweets);
   };
 
-  renderTweets(data);
+  loadTweets();
+  
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,34 @@
-$(document).ready(function () {
-  $('.new-tweet textarea').on('keyup', function (event) {
-    const { textLength } = $(this).context;
-    const count = $(this).parent().children('.counter');
+$(() => {
+  const validateForm = (formText) => {
+    if (!formText.length) {
+      alert('Don\'t try to submit a empty tweet 🙄');
+      return false;
+    }
+    if (formText.length > 140) {
+      alert('Your tweet is wayyyyyy too long 😓');
+      return false;
+    }
+    return true;
+  };
+  const textarea = $('.new-tweet textarea');
+  textarea.on('keyup', () => {
+    const textLength = textarea.val().length;
+    const count = textarea.parent().children('.counter');
     count.text(140 - textLength);
     140 - textLength < 0 ? count.css('color', 'red') : count.css('color', '');
   });
 
-  $('form').on('submit', function (event) {
+  const form = $('form');
+  form.on('submit', (event) => {
     event.preventDefault();
-    $.ajax('/tweets', {
-      method: "POST",
-      data: $(this).serialize()
-    }).then(response => console.log('response is', response));
+    const typedText = form.children('textarea').val();
+    console.log(typedText.length);
+    //validate form here needs to take in an argument
+    if (validateForm) {
+      $.ajax('/tweets', {
+        method: "POST",
+        data: form.serialize()
+      }).then(response => console.log('response is', response));
+    }
   });
 });
