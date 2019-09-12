7d251413491594c7b25446db2b885957f18a440b
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-12 03:13:28 +0000

    implemeneted scroll up button functionality

--- public/scripts/scroll-up.js
+++ public/scripts/scroll-up.js
@@ -1,9 +1,13 @@
 $(() => {
   const browser = $(document);
   const scrollUp = $('i.fa-angle-double-up');
+  const form = $('.new-tweet');
   browser.scroll(() => {
-    console.log(browser.scrollTop());
     if (browser.scrollTop() >= 400) scrollUp.css('display', 'block');
     else scrollUp.css('display', '');
+  });
+  scrollUp.on('click', () => {
+    form.slideDown();
+    browser.scrollTop(400);
   });
 });
